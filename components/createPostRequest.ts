8cb7017edb4d4487bf7ecc3bb4bb0978f2b52b9a
南極雄 <70533278+mcg25035@users.noreply.github.com>
2024-09-08 00:16:40 +0800

    implement method for createPostRqs

--- components/createPostRequest.ts
+++ components/createPostRequest.ts
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios"
+import exp from "constants";
 
 function getFileDataURL(fileInputElement: HTMLInputElement): Promise<string> {
   if (fileInputElement && fileInputElement.files && fileInputElement.files.length > 0) {
@@ -25,13 +27,34 @@ function getFileDataURL(fileInputElement: HTMLInputElement): Promise<string> {
   }
 }
 
+interface PostData {
+  title: string;
+  description: string;
+  imageBase64: string;
+}
 
-
-export default async function request(titleElement: HTMLInputElement, descriptionElement: HTMLInputElement, FileInputElement: HTMLInputElement, userId: string) {
+export async function getDataFromElement(titleElement: HTMLInputElement, descriptionElement: HTMLInputElement, FileInputElement: HTMLInputElement, userId: string) : Promise<PostData> {
   var title = titleElement.value;
   var description = descriptionElement.value;
   var imageBase64 = await getFileDataURL(FileInputElement);
   alert("title: " + title);
   alert("description: " + description);
   alert("imageBase64: " + imageBase64);
+  return {
+    title,
+    description,
+    imageBase64
+  };
+}
+
+export async function sendPostRequest(data: PostData, lng: number, lat: number, category: string) {
+  const response = await axios.post("/api/post", {
+    title: data.title,
+    description: data.description,
+    base64image: data.imageBase64,
+    lng,
+    lat,
+    category
+  });
+  return response.data;
 }
