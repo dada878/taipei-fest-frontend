f816b4e702917fc0e25401ac18412cb70f58abf3
rlongdragon <jz744335@gamil.com>
2024-09-08 00:47:03 +0800

    可發送貼文封包:D

--- components/post.tsx
+++ components/post.tsx
@@ -1,38 +1,56 @@
 'use client';
 import React, { useEffect, useRef } from "react";
-import style from "../public/use.module.css"
-// import qq from "../public/qq.png"
+import { Dispatch, SetStateAction } from 'react';
+import style from "./use.module.css"
 import Image from "next/image";
-export default function Post() {
-    const textareaElement = useRef(null);
+
+import { getDataFromElement, sendPostRequest } from './createPostRequest'
+
+
+
+export default function Post({ isOpen, setIsOpen }: { isOpen: boolean, setIsOpen: Dispatch<SetStateAction<boolean>> }) {
+    const descriptionElement = useRef(null);
+    const titleElement = useRef(null)
+    const fileInputElement = useRef(null)
+    // const descriptionElement = useRef(null)
     const [textareaContent, setTextareaContent] = React.useState("");
 
-    useEffect(function(){
-        if(!textareaElement.current) return; 
-        var currentElement: HTMLTextAreaElement = textareaElement.current;
-        
-        currentElement.oninput = function(){
+    useEffect(function () {
+        if (!descriptionElement.current) return;
+        var currentElement: HTMLTextAreaElement = descriptionElement.current;
+
+        currentElement.oninput = function () {
             console.log(currentElement.scrollHeight);
             setTextareaContent(currentElement.value);
             currentElement.style.height = "";
             currentElement.style.height = Math.min(currentElement.scrollHeight) + "px";
         }
     }, [])
-    
-    return(
-        <div className={style.all}>
+
+    async function handleClick() {
+        if (!titleElement.current) return
+        if (!descriptionElement.current) return
+        if (!fileInputElement.current) return
+
+        var postData = await getDataFromElement(titleElement.current, descriptionElement.current, fileInputElement.current, 'user')
+        await sendPostRequest(postData, 123.456, 456.789, "#global #A #B");
+    }
+
+    return (
+        <div className={style.all} style={{ display: (isOpen ? 'flex' : 'none') }}>
             <div className={style.bg}>
                 <div className={style.title}>
-                    <input type="text" placeholder="請輸入標題..."  name="title"></input>
+                    <input ref={titleElement} type="text" placeholder="請輸入標題..." name="title"></input>
                 </div>
                 <div className={style.des}>
-                    <textarea ref={textareaElement} placeholder="請輸入描述..." name="description"></textarea>
+                    <textarea ref={descriptionElement} placeholder="請輸入描述..." name="description"></textarea>
                 </div>
+                <input ref={fileInputElement} type="file" accept="image/png, image/gif, image/jpeg" />
                 <div className={style.pic}>
                     <Image src="/qq.png" alt="" width={300} height={300} />
                 </div>
                 <div className={style.butt}>
-                    <button onClick={()=>{}}>發布動態</button>
+                    <button onClick={handleClick}>發布動態</button>
                 </div>
             </div>
         </div>
