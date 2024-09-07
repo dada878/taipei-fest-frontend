7c68669fc732c796146f652ecaa09772514688dd
Jyun Lun Liao <alan9999tw@gmail.com>
2024-09-07 19:54:11 +0800

    新增圖片mark by long

---
+++ components/mark.tsx
@@ -0,0 +1,10 @@
+import style from './mark.module.css'
+
+export default function Mark() {
+    return <>
+        <div className={style.re}>
+            <div className={style.img}></div>
+            <div className={style.adv}></div>
+        </div>
+    </>
+}
